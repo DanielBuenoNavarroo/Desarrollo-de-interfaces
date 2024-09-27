4037ede1a2a5a3fb0cdf235c8afbcd02e48b2bfb
Daniel Bueno Navarro <danielbuenonavarro@gmail.com>
2024-09-27 21:03:50 +0200

    Cambios

---
+++ ejemplo-tipografias/src/main.ts
@@ -0,0 +1,18 @@
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>
+        Lorem ipsum odor amet, consectetuer adipiscing elit.
+    </h1>
+    <h2>
+        Lorem ipsum odor amet, consectetuer adipiscing elit.
+    </h2>
+    <h3>
+        Lorem ipsum odor amet, consectetuer adipiscing elit.
+    </h3>
+    <div>
+        <a href="#"><i class="fa-brands fa-linkedin rrss"></i></a>
+        <a href="#"><i class="fa-brands fa-linkedin rrss"></i></a>
+        <a href="#"><i class="fa-brands fa-linkedin rrss"></i></a>
+    </div>
+  </div>
+`;
